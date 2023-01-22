0af357a651e47398b276589f2fa8587ad413caf8
Dan7nilo <dan6nilo@gmail.com>
2023-01-22 18:08:50 -0300

    beta3
    Com menu de login funcional e pagina de perfil de compras, (falta ajustar a responsividade do menu de login para resoluções menores que 1278px )

--- JavaScript/script.js
+++ JavaScript/script.js
@@ -10,6 +10,26 @@ if(getComputedStyle(menu).display =="none"){
 else{
     menu.style.display = "none"
 }
+}
+function login($event){
+    let loginpage = document.getElementById("loginpage")
+   
+    if(getComputedStyle(loginpage).display =="none"){
+        loginpage.style.display = "flex"  
+    }
+    else{
+        loginpage.style.display = "none"
+    }
+}
 
+function logar(){
+    var login = document.getElementById("log").value;
+    var senha = document.getElementById("senha").value;
 
+    if(login == "admin" && senha == "admin"){
+        alert("Sucesso")
+        location.href = "/Html/compras.html"
+    }else{
+    alert("Usuario ou senha incorretos")
+    }
 }
